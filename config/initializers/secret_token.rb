f606d1f5b8e3849d155afc2771f7de3dc6b21cf6
Andy Brown <brownie3003@gmail.com>
2014-03-28 15:41:32 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PocketSocial::Application.config.secret_key_base = 'ca3da73f458876fd80c7e84a42d39a1b823e66ac53cf43788bb1e85720d489bf7ed4c52402bf9373c7b4162c61378144db301b03249082429f0267b9c02f0010'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PocketSocial::Application.config.session_store :cookie_store, key: '_pocket_social_session'
