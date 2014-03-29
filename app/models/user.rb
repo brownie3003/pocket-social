64bb6eaf497cbf3aac113f9d7821a25d76a22f0a
Andy Brown <brownie3003@gmail.com>
2014-03-29 15:14:48 +0000

    sign-in working

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User
   include ActiveModel::SecurePassword
 
   before_save { self.email = email.downcase }
+  before_create :create_remember_token
 
   field :username
   field :email
@@ -10,6 +11,7 @@ class User
   field :_id, type: String, default: ->{ username }
 
   field :password_digest
+  field :remember_token
 
   has_secure_password
 
@@ -19,4 +21,19 @@ class User
   validates_uniqueness_of :email, :username
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates_format_of :email , with: VALID_EMAIL_REGEX
+  
+  # Should probably add a salt if ever going to production
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.hash(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.hash(User.new_remember_token)
+    end
 end
