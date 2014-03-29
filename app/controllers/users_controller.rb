041d2d3b6d43a87f28979f37300d0dc4542f9ed4
Andy Brown <brownie3003@gmail.com>
2014-03-29 14:06:17 +0000

    implement has_secure_password successfully

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
-      if @user.update(user_params)
+      if @user.update_attributes(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { head :no_content }
       else
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:username, :email, :password)
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,22 @@
 class User
   include Mongoid::Document
+  include ActiveModel::SecurePassword
+
+  before_save { self.email = email.downcase }
+
   field :username
   field :email
   # Must use username as ID because .com of email doesn't work for routes
   field :_id, type: String, default: ->{ username }
-  field :password
+
+  field :password_digest
+
+  has_secure_password
+
   field :pocket, type: Hash
 
   validates_presence_of :email, :password
   validates_uniqueness_of :email, :username
-  # validates_format_of :email , with: /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates_format_of :email , with: VALID_EMAIL_REGEX
 end
