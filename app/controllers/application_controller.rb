4362f1013cfc457ceda48ceb1f761e5ea12547cb
Andy Brown <brownie3003@gmail.com>
2014-04-23 21:23:27 +0100

    change host config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   
   def default_url_options
     if Rails.env.production?
-      {:host => "http://pocket-social.herokuapp.com"}
+      {:host => "http://pocket-social.com"}
     else  
       {}
     end
