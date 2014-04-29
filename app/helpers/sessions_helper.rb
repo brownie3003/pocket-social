32d64ccbd10741ad965de32c198c8ef5ce2b42f9
Andy Brown <brownie3003@gmail.com>
2014-04-29 21:03:32 +0100

    something that hasn't helped user sign in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,16 +3,17 @@ module SessionsHelper
     def sign_in user
         remember_token = User.new_remember_token
         cookies.permanent[:remember_token] = remember_token
-        user.update_attribute(:remember_token, User.app_hash(remember_token))
+        user.update_attribute(:remember_token, User.digest(remember_token))
+        
         self.current_user = user
     end
     
-    def current_user= user
+    def current_user=(user)
         @current_user = user
     end
     
     def current_user
-        remember_token = User.app_hash(cookies[:remember_token])
+        remember_token = User.digest(cookies[:remember_token])
         @current_user ||= User.find_by(remember_token: remember_token)
     end
     
@@ -32,7 +33,7 @@ module SessionsHelper
     end
     
     def sign_out
-        current_user.update_attribute(:remember_token, User.app_hash(User.new_remember_token))
+        current_user.update_attribute(:remember_token, User.digest(User.new_remember_token))
         cookies.delete(:remember_token)
         self.current_user = nil
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -43,13 +43,13 @@ class User
   end
 
   # Mongoid has a has method so named it app_hash
-  def User.app_hash(token)
+  def User.digest(token)
     Digest::SHA1.hexdigest(token.to_s)
   end
 
   private
 
     def create_remember_token
-      self.remember_token = User.app_hash(User.new_remember_token)
+      self.remember_token = User.digest(User.new_remember_token)
     end
 end
