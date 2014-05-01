338cd0332cf25cd47a66047c7790a326d5335539
andyb <andyb@moo.com>
2014-05-01 16:49:55 +0100

    explicitly set current_user = user in session create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,8 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
             sign_in user
-            if !current_user.nil?
-                redirect_back_or user
-            else
-                flash.now[:error] = "Site error we couldn't sign you in"
-                render 'new'
-            end
+            current_user = user
+            redirect_back_or user
         else
             flash.now[:error] = 'Invalid email/password combination'
             render 'new'
