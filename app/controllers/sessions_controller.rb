7445f236da1368abedf2a4de29b44d06a6d204fa
andyb <andyb@moo.com>
2014-05-01 17:14:35 +0100

    Print out current_user in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
             sign_in user
-            current_user = user
+            puts "Current user in the session controller? #{current_user}"
             redirect_back_or user
         else
             flash.now[:error] = 'Invalid email/password combination'
