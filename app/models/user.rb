2e51e1492ebf1622cb5c398959876a60161df2e3
Andy Brown <brownie3003@gmail.com>
2014-05-02 23:58:07 +0100

    how am I still getting a hash?:

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,6 +50,6 @@ class User
   private
 
     def create_remember_token
-      self.remember_token = User.digest(User.new_remember_token)
+    #   self.remember_token = User.digest(User.new_remember_token)
     end
 end
