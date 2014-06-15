56dcf26040c8d9280a06d0f85d2bc95a99cab68d
Andy Brown <brownie3003@gmail.com>
2014-06-15 08:49:34 +0100

    recommendations just require ajax

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -17,6 +17,11 @@ class WelcomeController < ApplicationController
             @user.twitter_following.each do |uid|
                 @twitter_following << User.find_by(uid: uid)
             end
+            
+            # If they don't have any recommendations from Twitter
+            if @twitter_following.empty?
+                @popular_users = [User.find_by(id: 'mattclifford'), User.find_by(id: "nicangeli"), User.find_by(id: "UBC_founder")]
+            end
         end
         current_user.update_attributes(new_user?: false)
     end
